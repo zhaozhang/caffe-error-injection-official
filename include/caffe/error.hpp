65e7da589e4208ecacea1b9d4233d878d699b223
lei huang <wiliam_huang@hotmail.com>
2018-03-13 09:13:39 -0500

    Add error.hpp.

---
+++ include/caffe/error.hpp
@@ -0,0 +1,15 @@
+int step_cur=-1;
+__thread int deviceid=-1;
+__thread int Active=0;
+int mut_step=-1;
+int mut_layer_fp=-1;
+int mut_layer_bp=-1;
+int mut_param_set=-1;
+int mut_layer_fp_idx=-1;
+int mut_layer_bp_idx=-1;
+int mut_param_set_idx=-1;
+int mut_bit=-1;
+int bit_mask[32];
+int Clamp_On=0;
+float Data_Range=1.0E10;
+int Is_In_Test=0;
